ff4aeaa0326e14e403fd7e77de24b836282c892c
kajugadaniels <kajugadaniels@gmail.com>
2024-04-25 11:27:12 +0200

    Implement auth with clerk

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,8 +1,9 @@
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
+import { Button } from "../ui/button"
 import NavItems from "./NavItems"
 import MobileNav from "./MobileNav"
-import { Button } from "../ui/button"
 
 const Header = () => {
     return (
@@ -15,17 +16,29 @@ const Header = () => {
                     />
                 </Link>
 
-                <nav className="md:flex-between hidden w-full max-w-xs">
-                    <NavItems />
-                </nav>
+                <SignedIn>
+                    <nav className="md:flex-between hidden w-full max-w-xs">
+                        <NavItems />
+                    </nav>
+                </SignedIn>
 
                 <div className="flex w-32 justify-end gap-3">
-                    <MobileNav />
-                    <Button asChild className="rounded-full" size="lg">
-                        <Link href="/sign-in">
-                            Login
-                        </Link>
-                    </Button>
+                    <SignedIn>
+                        <UserButton afterSignOutUrl="/" />
+                        <MobileNav />
+                    </SignedIn>
+                    <SignedOut>
+                        <Button asChild className="rounded-full" size="lg">
+                            <Link href="/sign-in">
+                                Login
+                            </Link>
+                        </Button>
+                        <Button asChild className="rounded-full" size="lg">
+                            <Link href="/sign-up">
+                                Register
+                            </Link>
+                        </Button>
+                    </SignedOut>
                 </div>
             </div>
         </header>
