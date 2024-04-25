aa5b8c66f61ad4e13f785d0c37e53470dc18f252
kajugadaniels <kajugadaniels@gmail.com>
2024-04-25 10:25:29 +0200

    Stylize header components

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,21 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body className={poppins.variable}>{children}</body>
+            <head>
+                {/* Link to Google Fonts */}
+                <link
+                    href={`https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap`}
+                    rel="stylesheet"
+                />
+                <style>
+                    {`
+                        body {
+                            font-family: 'Poppins', sans-serif; /* Apply Poppins font */
+                        }
+                    `}
+                </style>
+            </head>
+            <body>{children}</body>
         </html>
     )
 }
