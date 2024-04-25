0d48e54e2995d254346deefd44115394b24718b5
kajugadaniels <kajugadaniels@gmail.com>
2024-04-25 18:34:56 +0200

    Implement Zod validation

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -2,10 +2,13 @@
 
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
-import * as z from 'zod'
 import { Button } from "@/components/ui/button"
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { eventFormSchema } from "@/lib/validator"
+import * as z from "zod"
+import { eventDefaultValues } from "@/constants"
+import Dropdown from "./Dropdown"
 
 type EventFormProps = {
     userId: string
@@ -13,45 +16,56 @@ type EventFormProps = {
 }
 
 const EventForm = ({ userId, type }: EventFormProps) => {
-    const formSchema = z.object({
-        username: z.string().min(2, {
-            message: "Username must be at least 2 characters.",
-        }),
-    })
+    const initialValues = event && type === 'Update' 
+    ? { 
+        ...event, 
+        startDateTime: new Date(event.startDateTime), 
+        endDateTime: new Date(event.endDateTime) 
+    }
+    : eventDefaultValues;
 
     // 1. Define your form.
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-        username: "",
-        },
+    const form = useForm<z.infer<typeof eventFormSchema>>({
+        resolver: zodResolver(eventFormSchema),
+        defaultValues: initialValues
     })
     
     // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof formSchema>) {
+    function onSubmit(values: z.infer<typeof eventFormSchema>) {
         // Do something with the form values.
         // ✅ This will be type-safe and validated.
         console.log(values)
     }
+
     return (
         <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                <FormField
-                    control={form.control}
-                    name="username"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Username</FormLabel>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5">
+                <div className="flex flex-col gap-2 md:flex-row">
+                    <FormField
+                        control={form.control}
+                        name="title"
+                        render={({ field }) => (
+                        <FormItem className="w-full">
                             <FormControl>
-                                <Input placeholder="shadcn" {...field} />
+                                <Input placeholder="Event title" {...field} />
                             </FormControl>
-                            <FormDescription>
-                                This is your public display name.
-                            </FormDescription>
                             <FormMessage />
                         </FormItem>
-                    )}
-                />
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="categoryId"
+                        render={({ field }) => (
+                        <FormItem className="w-full">
+                            <FormControl>
+                                <Dropdown onChangeHandler={field.onChange} value={field.value} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                        )}
+                    />
+                </div>
                 <Button type="submit">Submit</Button>
             </form>
         </Form>
